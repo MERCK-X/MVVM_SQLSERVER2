6fd4073df4764441e915629ed98522a8ac492cb9
MERCK1 <clementemonroymartinez@gmail.com>
2025-03-31 21:38:00 -0600

    Agregar archivos de proyecto.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MVVM_SQLSERVER;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
